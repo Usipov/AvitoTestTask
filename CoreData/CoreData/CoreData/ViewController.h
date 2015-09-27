466695db8dbac6bdb0438e27682106cfe3941f42
Timur Yusipov <timur.yusipov@customertimes.com>
2015-09-28 00:30:02 +0300

    Initial Commit

---
+++ CoreData/CoreData/CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Tim on 28.09.15.
+//  Copyright (c) 2015 YusipovTimur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
